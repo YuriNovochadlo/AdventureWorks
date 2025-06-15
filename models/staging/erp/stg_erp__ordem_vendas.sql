b5bc8dcf2860f3f9853c8a8b1c5f32929a6b6c56
Yuri Novochadlo <yuri.novochadlo@gmail.com>
2025-06-15 10:11:59 +0000

    Relacionando na camada stagin o endereço que o pedido foi enviado

--- models/staging/erp/stg_erp__ordem_vendas.sql
+++ models/staging/erp/stg_erp__ordem_vendas.sql
@@ -7,7 +7,7 @@ with
         select
         cast(salesorderid as int) as pk_id_pedido
         ,cast(customerid as int) as fk_id_cliente_pedido
-        ,cast(territoryid as int) as fk_id_localizacao_pedido
+        ,cast(SHIPTOADDRESSID as int) as fk_id_localizacao_pedido
         ,cast(creditcardid as int) as fk_id_cartao_de_credito
         ,cast(orderdate as date) as data_pedido
         ,cast(shipdate as date) as data_envio_pedido
